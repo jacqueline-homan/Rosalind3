04442ce32f3b3d40f7672ec0fe55fd35f3c954ba
jacqueline-homan <jacquelinehoman7@gmail.com>
2016-04-03 09:06:55 -0400

    solved Rosalind DNA problem no. 3

---
+++ Program.fs
@@ -0,0 +1,27 @@
+﻿open System
+open System.IO
+
+let stringReverse (s: string) =
+    String(Array.rev (s.ToCharArray()))
+
+let reverseComplimentDNA =
+    String.map(function
+        | 'T' -> 'A'
+        | 'A' -> 'T'
+        | 'C' -> 'G'
+        | 'G' -> 'C'
+        | x -> x)
+
+[<EntryPoint>]
+let main argv = 
+    let path = @"/home/jacque/Downloads/rosalind_revc.txt"
+
+    let file = File.ReadAllText(path)
+    //let file = "AAAACCCGGT"
+    file
+    |> stringReverse
+    |> reverseComplimentDNA
+    |> printfn "%s" 
+
+    0 // return an integer exit code
+
---
+++ Script3.fsx
@@ -0,0 +1,6 @@
+﻿open System
+
+let myArray = [|1;2;3;4|]
+myArray
+|> Array.rev
+|> Array.iter(printf "%A")
